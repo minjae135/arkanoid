9fef3572bbe408a3a097011fb27faeb9a6e01eb0
손민재 <minjaeson443@naver.com>
2026-04-18 18:19:53 +0900

    어드민 비번 교체 ver2

--- js/admin-secrets.js
+++ js/admin-secrets.js
@@ -1,7 +1,7 @@
 // This file is auto-generated. Do not edit manually.
 export const ADMIN_HASHES = {
-    RANK_1: "f44e64e75f3948e9f73f8dfa94721c4ce8cbb4f265c4790c702b2d41cfbf2753",
+    RANK_1: "ba1b60b579e28da6e4c21893dfdf4b1756f8431142c49910bcb8324146b1d1df",
     RANK_2: "d3037702b0f4d33fc163929c1cfadf20ea3ad5e24d3d47b845683321f13f7eb5",
-    RANK_3: "970eaf0d4dc9bec3d74134fa3d69e6f8025c03ef82a83fd5c7782e3fe70075e7"
+    RANK_3: "dfedbcdff47731651eab3ff6cf271bdb715a3e06927dd21a7298ae72fb8c707c"
 };
 //# sourceMappingURL=admin-secrets.js.map
